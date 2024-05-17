24a0a80bffd837209656561dea50359373b333da
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-05-17 14:04:00 +0200

    Player is no longer flying off screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public GameObject player;
     public float playerSpeed;
     private float movementHorizontal;
+    public float maxX;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movementHorizontal = Input.GetAxis("Horizontal");
-        transform.position += Vector3.right * movementHorizontal * playerSpeed * Time.deltaTime;
+
+        if ((movementHorizontal > 0 && transform.position.x < maxX) || (movementHorizontal < 0 && transform.position.x > - maxX))
+        {
+            transform.position += Vector3.right * movementHorizontal * playerSpeed * Time.deltaTime;
+        }
     }
 }
