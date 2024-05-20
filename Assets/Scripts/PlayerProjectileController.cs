d05760ba677a746f1a15ed57b64e913f5d0b6afd
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-05-20 12:02:14 +0200

    Added PlayerProjectileController with projectile shoot logic

---
+++ Assets/Scripts/PlayerProjectileController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerProjectileController : MonoBehaviour
+{
+    public float projectileSpeed = -3;
+    public GameObject playerProjectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, projectileSpeed * Time.deltaTime, 0));
+    }
+}
