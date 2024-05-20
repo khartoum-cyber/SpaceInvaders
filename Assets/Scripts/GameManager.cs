5e5fcbf9ce03b1533ee4ae78dff8a8eb42b4f5ae
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-05-20 12:42:11 +0200

    Added variables for enemies / set up for enemies grid

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Vector2Int size;
+    public Vector2 offset;
+    public GameObject enemy;
+    private GameObject newEnemy;
+    public GameObject enemies;
+    public int enemyCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
