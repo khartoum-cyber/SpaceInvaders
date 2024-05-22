9c647dcc7ec22a90874d1424f074694aec19d33f
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-05-22 10:29:03 +0200

    Added enemy projectile prefab

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -7,6 +8,11 @@ public class EnemyController : MonoBehaviour
     private float timer = 0;
     private int numOfMovements = 0;
     private float movementAmount = 0.19f;
+    public GameObject enemyProjectile;
+    public GameObject enemyProjectileClone;
+    public GameObject enemy;
+    private float fireTimer = 0;
+    private float timeToFire = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +25,14 @@ public class EnemyController : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-<<<<<<< HEAD
+
         // Move enemies horizontally
-=======
->>>>>>> 4b0ec850f81e8fb445733bc8036a89a6990e1bf1
         if (timer > 0.5 && numOfMovements != 17)
         {
             transform.Translate(new Vector3(movementAmount, 0, 0));
             timer = 0;
             numOfMovements++;
         }
-<<<<<<< HEAD
 
         // Move enemies vertically and reverse horizontal movement direction
         if (numOfMovements == 17)
@@ -39,7 +42,5 @@ public class EnemyController : MonoBehaviour
             timer = 0;
             movementAmount = -movementAmount;
         }
-=======
->>>>>>> 4b0ec850f81e8fb445733bc8036a89a6990e1bf1
     }
 }
