8e0c28fee959460352da750b0912d438c80420c9
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-05-22 11:15:45 +0200

    Enemy projectiles move and destroy player object on collision but still collide with enemy objects

--- Assets/Scripts/EnemyProjectileController.cs
+++ Assets/Scripts/EnemyProjectileController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyProjectileController : MonoBehaviour
 {
     public float enemyProjectileSpeed = -3;
+    public GameObject enemyProjectile;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,15 @@ public class EnemyProjectileController : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(0, enemyProjectileSpeed * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // destroys player object
+            collision.gameObject.SetActive(false);
+            Destroy(enemyProjectile);
+        }
     }
 }
